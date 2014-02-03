daa62ef5dd37d6364328658da6b44255a18de0e8
Jonas Eriksson <jonas@programenta.se>
2014-02-04 00:52:30 +0100

    Working example

---
+++ PersistenceHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Isam.Esent.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalrDataSelfHost
+{
+    public class PersistenceHub : Hub
+    {
+        public string Get(string key)
+        {
+            return Persistence.Get("default", key);
+        }
+        public void Set(string key, object value)
+        {
+            Persistence.Set("default", key, value);
+        }
+        public IDictionary<string,string> All()
+        {
+            return Persistence.All("default");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Isam.Esent.Collections.Generic;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.IO;
+
+namespace SignalrDataSelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration();                
+                map.RunSignalR(hubConfiguration);
+            });
+                
+            string contentPath = Path.Combine(Environment.CurrentDirectory, @"..\..");
+            var baseUrl = "";
+            app.UseStaticFiles(new Microsoft.Owin.StaticFiles.StaticFileOptions()
+            {
+                RequestPath = new PathString(baseUrl),
+                FileSystem = new PhysicalFileSystem(contentPath)
+            });
+        }
+    }
+
+}
